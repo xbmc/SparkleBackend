828e264d6533ac42072372651e96b74c5ac32d7e
Memphiz <memphis@machzwo.de>
2017-06-07 23:08:53 +0200

    fixups

--- add_version.sh
+++ add_version.sh
@@ -19,6 +19,16 @@ if [[ $FILENAME == *"dmg"* ]]; then
   SUBFOLDER=osx/x86_64
 fi
 
+if [ $OS == "windows-x86" ]
+then
+  SUBFOLDER=win32
+fi
+
+if [ $OS == "windows-x64" ]
+then
+  SUBFOLDER=win64
+fi
+
 if [ -z $SUBFOLDER ]
 then
   echo "Release subfolder could not be determined from given filename. Aborting..."
@@ -27,18 +37,21 @@ fi
 
 FULLPATH=/var/www/downloads/releases/$SUBFOLDER/$FILENAME
 
-openssl=/usr/bin/openssl
-if [ $SPARKLE_PRIVATE_KEY_PATH ] && [ -e $SPARKLE_PRIVATE_KEY_PATH ]
+if [ $OS == "osx" ]
 then
-  echo Signing $FULLPATH
-  $openssl dgst -sha1 -binary < "$FULLPATH" | $openssl dgst -dss1 -sign "$SPARKLE_PRIVATE_KEY_PATH" | $openssl enc -base64 > signature.base64
-else
-  echo "SPARKLE_PRIVATE_KEY_PATH is not valid in node environment variables - dmg is not signed and can't be used for sparkle updates"
-  exit 3
+  openssl=/usr/bin/openssl
+  if [ $SPARKLE_PRIVATE_KEY_PATH ] && [ -e $SPARKLE_PRIVATE_KEY_PATH ]
+  then
+    echo Signing $FULLPATH
+    $openssl dgst -sha1 -binary < "$FULLPATH" | $openssl dgst -dss1 -sign "$SPARKLE_PRIVATE_KEY_PATH" | $openssl enc -base64 > signature.base64
+  else
+    echo "SPARKLE_PRIVATE_KEY_PATH is not valid in node environment variables - dmg is not signed and can't be used for sparkle updates"
+    exit 3
+  fi
 fi
 
 DSASIGNATURE=`cat signature.base64`
-FILESIZE=`du -b "$FULLPATH" | cut -f1`
+FILESIZE=`ls -al "$FULLPATH" | awk '{print $5}'`
 
 #generating the item
 sed "s|#TITLE#|$TITLE|" sparkle_item.template | \
@@ -47,9 +60,20 @@ sed "s|#VERSION#|$VERSION|" | \
 sed "s|#SUBFOLDER#|$SUBFOLDER|" | \
 sed "s|#FILENAME#|$FILENAME|" | \
 sed "s|#DATE#|$DATE|" | \
-sed "s|#FILESIZE#|$FILESIZE|" | \
-sed "s|#OS#|$OS|" |
-sed "s|#DSASIGNATURE#|$DSASIGNATURE|" > new_item.xml
+sed "s|#FILESIZE#|$FILESIZE|" | 
+sed "s|#OS#|$OS|" > new_item.xml
+
+if [ $OS == "osx" ]
+then
+  cp new_item.xml new_item2.xml
+  cat new_item2.xml | sed "s|#DSASIGNATURE#|$DSASIGNATURE|" > new_item.xml
+  rm new_item2.xml
+else
+  #winsparkle needs the signature attribute removed
+  cp new_item.xml new_item2.xml    
+  cat new_item2.xml | sed "s|sparkle:dsaSignature.*\/|\/|" > new_item.xml
+  rm new_item2.xml
+fi
 
 cat sparkle_xmlfeed_start.template > $SPARKLE_XML
 cat new_item.xml >> $SPARKLE_XML
