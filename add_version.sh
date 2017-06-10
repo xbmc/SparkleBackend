ece281217028671770216add6cab359705589b9b
Memphiz <memphis@machzwo.de>
2017-06-10 19:42:46 +0200

    Add new items to the appfeed instead of rewriting the feed everytime

--- add_version.sh
+++ add_version.sh
@@ -48,21 +48,29 @@ FULLPATH="./$FILENAME"
 
 curl -L $WEBPATH -o $FULLPATH
 
+
+SIGNATURE_FILE=signature.base64
+
 if [ "$OS" = "osx" ]
 then
   openssl=/usr/bin/openssl
   if [ $SPARKLE_PRIVATE_KEY_PATH ] && [ -e $SPARKLE_PRIVATE_KEY_PATH ]
   then
     echo Calculating signature for $FULLPATH
-    $openssl dgst -sha1 -binary < "$FULLPATH" | $openssl dgst -dss1 -sign "$SPARKLE_PRIVATE_KEY_PATH" | $openssl enc -base64 > signature.base64
+    $openssl dgst -sha1 -binary < "$FULLPATH" | $openssl dgst -dss1 -sign "$SPARKLE_PRIVATE_KEY_PATH" | $openssl enc -base64 > $SIGNATURE_FILE
   else
     echo "SPARKLE_PRIVATE_KEY_PATH is not valid in node environment variables - dmg signature can't be calculated and can't be used for sparkle updates"
     exit 3
   fi
 fi
 
-DSASIGNATURE=`cat signature.base64`
+DSASIGNATURE=`cat $SIGNATURE_FILE`
 FILESIZE=`ls -al "$FULLPATH" | awk '{print $5}'`
+
+
+NEW_ITEM_TMP_FILE1=new_item.xml
+NEW_ITEM_TMP_FILE2=new_item2.xml
+OLD_ITEMS_TMP_FILE=old_items.xml
 
 #generating the item
 sed "s|#TITLE#|$TITLE|" sparkle_item.template | \
@@ -72,20 +80,41 @@ sed "s|#SUBFOLDER#|$SUBFOLDER|" | \
 sed "s|#FILENAME#|$FILENAME|" | \
 sed "s|#DATE#|$DATE|" | \
 sed "s|#FILESIZE#|$FILESIZE|" | 
-sed "s|#OS#|$OS|" > new_item.xml
+sed "s|#OS#|$OS|" > $NEW_ITEM_TMP_FILE1
 
 if [ "$OS" = "osx" ]
 then
-  cp new_item.xml new_item2.xml
-  cat new_item2.xml | sed "s|#DSASIGNATURE#|$DSASIGNATURE|" > new_item.xml
-  rm new_item2.xml
+  cp $NEW_ITEM_TMP_FILE1 $NEW_ITEM_TMP_FILE2
+  cat $NEW_ITEM_TMP_FILE2 | sed "s|#DSASIGNATURE#|$DSASIGNATURE|" > $NEW_ITEM_TMP_FILE1
+  rm $NEW_ITEM_TMP_FILE2
 else
   #winsparkle needs the signature attribute removed
-  cp new_item.xml new_item2.xml    
-  cat new_item2.xml | sed "s|sparkle:dsaSignature.*\/|\/|" > new_item.xml
-  rm new_item2.xml
+  cp $NEW_ITEM_TMP_FILE1 $NEW_ITEM_TMP_FILE2    
+  cat $NEW_ITEM_TMP_FILE2 | sed "s|sparkle:dsaSignature.*\/|\/|" > $NEW_ITEM_TMP_FILE1
+  rm $NEW_ITEM_TMP_FILE2
 fi
 
+# extract the current items first from the sparklexml file
+
+HEADER_LINE1=".*\<?xml.*"
+HEADER_LINE2=".*\<rss.*"
+HEADER_LINE3="\<channel\>"
+FOOTER_LINE1="\<\/channel\>"
+FOOTER_LINE2="\<\/rss\>"
+sed -ie "/$HEADER_LINE1/d" $SPARKLE_XML
+sed -ie "/$HEADER_LINE2/d" $SPARKLE_XML
+sed -ie "/$HEADER_LINE3/d" $SPARKLE_XML
+sed -ie "/$FOOTER_LINE1/d" $SPARKLE_XML
+sed -ie "/$FOOTER_LINE2/d" $SPARKLE_XML
+
+mv $SPARKLE_XML $OLD_ITEMS_TMP_FILE
+
 cat sparkle_xmlfeed_start.template > $SPARKLE_XML
-cat new_item.xml >> $SPARKLE_XML
+cat $NEW_ITEM_TMP_FILE1 >> $SPARKLE_XML
+cat $OLD_ITEMS_TMP_FILE >> $SPARKLE_XML
 cat sparkle_xmlfeed_end.template >> $SPARKLE_XML
+
+#some cleanup
+rm $OLD_ITEMS_TMP_FILE
+rm $NEW_ITEM_TMP_FILE1
+rm $SIGNATURE_FILE
